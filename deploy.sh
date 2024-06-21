81f53e4cb239b6cccc22804ccd89a3ef5444c0af
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2024-06-21 14:39:20 +0300

    fixiz

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,11 @@
 
 dir="$(realpath "$(dirname "$0")")"
 
-if [[ "$*" =~ r ]]; then
-  echo "Will Rebuild"
-  sudo docker system prune -a -f
-fi
-
 # cleanup
 cleanup() {
     echo "Performing cleanup..."
     rm -f "$dir/.env"
-    rm -rf "$dir/options_butterfly_condor"
+    rm -rf "$dir/environment/bot"
 }
 
 # Set up exit trap
@@ -21,36 +16,43 @@ trap cleanup EXIT SIGINT SIGTERM
 # .env setup
 cd "$dir"
 PORT=8888
-# make .env if not available
-if [ ! -e "environment/.env" ]; then
-  read -rp "Enter USERNAME: " tws_userid
-  read -rp "Enter PASSWORD: " tws_password
-  echo "USERNAME=$tws_userid" >> environment/.env
-  echo "PASSWORD=$tws_password" >> environment/.env
-fi
+bot_repo="git@github.com:Lumiwealth-Strategies/options_butterfly_condor.git"
+bot_name="$(basename "$bot_repo" .git)"
 
-# pick trade mode
-echo "Select trading mode:"
-echo "[1] Live"
-echo "[2] Paper"
-read -rp "Enter your choice: " choice
+while [ "$choice" != "1" ] && [ "$choice" != "2" ]; do
+  echo "Menu:"
+  echo "[1] Trade Live"
+  echo "[2] Trade Paper"
+  echo "[3] Reset Credentials"
+  echo "[4] Docker Prune Everything"
+  
+  read -rp "Enter your choice: " choice
 
-if [ "$choice" == "1" ]; then
-    echo "[*] Will Deploy Live"
-    printf "TRADING_MODE=live\n" >> .env
+  if [ "$choice" == "1" ]; then
+      echo "[*] Will Deploy Live"
+      printf "TRADING_MODE=live\n" >> .env
 
-elif [ "$choice" == "2" ]; then
-    echo "[*] Will Deploy Paper"
-    printf "TRADING_MODE=paper\n" >> .env
+  elif [ "$choice" == "2" ]; then
+      echo "[*] Will Deploy Paper"
+      printf "TRADING_MODE=paper\n" >> .env
 
-else
-    echo "Invalid choice. Exiting..."
-    exit 1
-    
-fi
+  elif [ "$choice" == "3" ]; then
+      rm -f "environment/.env"
+      echo "[*] Credentials Reset"
+
+  elif [ "$choice" == "4" ]; then
+    sudo docker system prune -a -f
+    echo "[*] Done"
+
+  else
+      echo "Invalid choice. Exiting..."
+      exit 1
+      
+  fi
+done
 
 # free ports
-sudo docker ps --format "{{.Names}}" | grep 'options-butterfly-condor' | xargs -r sudo docker kill > /dev/null
+sudo docker ps --format "{{.Names}}" | grep "$bot_name" | xargs -r sudo docker kill > /dev/null
 sudo docker ps --format "{{.Names}}" | grep 'ib-gateway' | xargs -r sudo docker kill > /dev/null
 
 # Check if port is in use and find an available port if necessary
@@ -59,6 +61,14 @@ while lsof -ti:$PORT > /dev/null; do
     exit 1
 done
 echo "Using port $PORT."
+
+# make .env if not available
+if [ ! -e "environment/.env" ]; then
+  read -rp "Enter USERNAME: " tws_userid
+  read -rp "Enter PASSWORD: " tws_password
+  echo "USERNAME=$tws_userid" >> environment/.env
+  echo "PASSWORD=$tws_password" >> environment/.env
+fi
 
 printf "INTERACTIVE_BROKERS_CLIENT_ID=%s\n" "$((RANDOM % 1000 + 1))" >> .env
 printf "INTERACTIVE_BROKERS_PORT=%s\n" "$PORT" >> .env
@@ -75,22 +85,22 @@ printf "READ_ONLY_API=no\n" >> .env
 # add secrets from .env to local .env
 cat "$dir/environment/.env" >> .env
 
-git clone "git@github.com:Lumiwealth-Strategies/options_butterfly_condor.git" || { echo "Probably not logged into git. Exiting..."; exit 1; }
+git clone "$bot_repo" "$dir/environment/bot" || { echo "Probably not logged into git. Exiting..."; exit 1; }
 
 # add needed files
-cp environment/Dockerfile options_butterfly_condor/
-cp environment/requirements.txt options_butterfly_condor/
-cp environment/healthcheck.py options_butterfly_condor/
-cp environment/launch.sh options_butterfly_condor/
+cp environment/Dockerfile "$dir/environment/bot/"
+cp environment/requirements.txt "$dir/environment/bot/"
+cp environment/healthcheck.py "$dir/environment/bot/"
+cp environment/launch.sh "$dir/environment/bot/"
 
 # add retries to bot ib connection
 OS="$(uname)"
 case $OS in
   'Linux')
-    sed -i 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/options_butterfly_condor/credentials.py"
+    sed -i 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/environment/bot/credentials.py"
     ;;
   'Darwin') 
-    sed -i '' 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/options_butterfly_condor/credentials.py"
+    sed -i '' 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/environment/bot/credentials.py"
     ;;
   *) 
   exit 1
