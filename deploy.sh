6adac73b8b9799c9a2074310f7cdfa64f23ebf32
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2024-07-03 01:33:22 +0300

    mighty stability improvements and more advanced options

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
+set -e
 
 dir="$(realpath "$(dirname "$0")")"
 
 # cleanup
 cleanup() {
     echo "Performing cleanup..."
-    rm -f "$dir/.env"
     rm -rf "$dir/environment/bot"
 }
 
@@ -15,10 +15,16 @@ trap cleanup EXIT SIGINT SIGTERM
 
 # .env setup
 cd "$dir"
-PORT=8888
 bot_repo="git@github.com:Lumiwealth-Strategies/options_condor_martingale.git"
 #bot_repo="git@github.com:Lumiwealth-Strategies/options_butterfly_condor.git"
 bot_name="$(basename "$bot_repo" .git)"
+
+# free ports
+sudo docker ps --format "{{.Names}}" | grep 'strategy' | xargs -r sudo docker kill > /dev/null
+sudo docker ps --format "{{.Names}}" | grep 'ib-gateway' | xargs -r sudo docker kill > /dev/null
+
+# reset .env if any
+rm -f "$dir/.env"
 
 while [ "$choice" != "1" ] && [ "$choice" != "2" ]; do
   echo "Menu:"
@@ -26,24 +32,37 @@ while [ "$choice" != "1" ] && [ "$choice" != "2" ]; do
   echo "[2] Trade Paper"
   echo "[3] Reset Credentials"
   echo "[4] Docker Prune Everything"
+  echo "[5] Docker Prune Strategies"
+  echo "[6] Containerize"
   
   read -rp "Enter your choice: " choice
 
   if [ "$choice" == "1" ]; then
       echo "[*] Will Deploy Live"
       printf "TRADING_MODE=live\n" >> .env
+      PORT=4003
 
   elif [ "$choice" == "2" ]; then
       echo "[*] Will Deploy Paper"
       printf "TRADING_MODE=paper\n" >> .env
+      PORT=4004
 
   elif [ "$choice" == "3" ]; then
       rm -f "environment/.env"
       echo "[*] Credentials Reset"
 
   elif [ "$choice" == "4" ]; then
-    sudo docker system prune -a -f
+    sudo docker system prune -a -f --volumes
     echo "[*] Done"
+
+  elif [ "$choice" == "5" ]; then
+    sudo docker images --format "{{.Repository}}" | grep "strategy" | xargs -r sudo docker rmi -f
+    echo "[*] Done"
+
+
+  elif [ "$choice" == "6" ]; then
+    #containerize=1
+    echo "[*] Will Containerize. It may use more memory,"
 
   else
       echo "Invalid choice. Exiting..."
@@ -51,10 +70,6 @@ while [ "$choice" != "1" ] && [ "$choice" != "2" ]; do
       
   fi
 done
-
-# free ports
-sudo docker ps --format "{{.Names}}" | grep "$bot_name" | xargs -r sudo docker kill > /dev/null
-sudo docker ps --format "{{.Names}}" | grep 'ib-gateway' | xargs -r sudo docker kill > /dev/null
 
 # Check if port is in use and find an available port if necessary
 while lsof -ti:$PORT > /dev/null; do
@@ -65,47 +80,52 @@ echo "Using port $PORT."
 
 # make .env if not available
 if [ ! -e "environment/.env" ]; then
-  read -rp "Enter USERNAME: " tws_userid
-  read -rp "Enter PASSWORD: " tws_password
-  echo "USERNAME=$tws_userid" >> environment/.env
-  echo "PASSWORD=$tws_password" >> environment/.env
+  read -rp "Enter TWS_USERID: " tws_userid
+  read -rp "Enter TWS_PASSWORD: " tws_password
+  read -rp "Enter ALPACA_API_KEY: " alpaca_api_key
+  read -rp "Enter ALPACA_API_SECRET: " alpaca_api_secret
+  echo "ALPACA_API_KEY=$alpaca_api_key" >> environment/.env
+  echo "ALPACA_API_SECRET=$alpaca_api_secret" >> environment/.env
+  echo "TWS_USERID=$tws_userid" >> environment/.env
+  echo "TWS_PASSWORD=$tws_password" >> environment/.env
+
+  echo 'ALPACA_BASE_URL="https://paper-api.alpaca.markets/v2"' >> environment/.env
+  printf 'NOT_USING_ALPACA=False' >> environment/.env
 fi
+
 
 printf "INTERACTIVE_BROKERS_CLIENT_ID=%s\n" "$((RANDOM % 1000 + 1))" >> .env
 printf "INTERACTIVE_BROKERS_PORT=%s\n" "$PORT" >> .env
-printf "INTERACTIVE_BROKERS_IP=ib-gateway\n" >> .env
-printf "TWOFA_TIMEOUT_ACTION=restart\n" >> .env
-printf "AUTO_RESTART_TIME=11:59 PM\n" >> .env
-printf "RELOGIN_AFTER_2FA_TIMEOUT=yes\n" >> .env
-printf "TIME_ZONE=Europe/Zurich\n" >> .env
-printf "TWS_ACCEPT_INCOMING=accept\n" >> .env
-printf "BYPASS_WARNING=yes\n" >> .env
-printf "VNC_SERVER_PASSWORD=12345678\n" >> .env
-printf "READ_ONLY_API=no\n" >> .env
 
 # add secrets from .env to local .env
 cat "$dir/environment/.env" >> .env
 
-git clone "$bot_repo" "$dir/environment/bot" || { echo "Probably not logged into git. Exiting..."; exit 1; }
+printf "INTERACTIVE_BROKERS_IP=ib-gateway\n" >> .env
 
-# add needed files
-cp environment/requirements.txt "$dir/environment/bot/"
-cp environment/Dockerfile "$dir/environment/bot/"
-cp environment/healthcheck.py "$dir/environment/bot/"
-cp environment/launch.sh "$dir/environment/bot/"
+if ! docker images --format "{{.Repository}}" | grep "strategy" > /dev/null 2>&1; then
+  git clone "$bot_repo" "$dir/environment/bot" || { echo "Probably not logged into git. Exiting..."; exit 1; }
 
-# add retries to bot ib connection
-OS="$(uname)"
-case $OS in
-  'Linux')
-    sed -i 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/environment/bot/credentials.py"
-    ;;
-  'Darwin') 
-    sed -i '' 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' "$dir/environment/bot/credentials.py"
-    ;;
-  *) 
-  exit 1
-  ;;  
-esac
+  # add needed files
+  cp environment/requirements.txt "environment/bot/"
+  cp environment/Dockerfile "environment/bot/"
+  cp environment/healthcheck.py "environment/bot/"
+  cp environment/launch.sh "environment/bot/"
 
-sudo docker compose up --remove-orphans -d
+  # patch credentials
+  OS="$(uname)"
+  case $OS in
+    'Linux')
+      sed -i 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' environment/bot/credentials.py
+      sed -i 's/if ALPACA_CONFIG\["API_KEY"\]:/if ALPACA_CONFIG["API_KEY"] and not os.environ.get("NOT_USING_ALPACA", "").lower() == "true":/' environment/bot/credentials.py
+      ;;
+    'Darwin') 
+      sed -i '' 's/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG)/broker = InteractiveBrokers(INTERACTIVE_BROKERS_CONFIG, max_connection_retries=50)/' environment/bot/credentials.py
+      sed -i '' 's/if ALPACA_CONFIG\["API_KEY"\]:/if ALPACA_CONFIG["API_KEY"] and not os.environ.get("NOT_USING_ALPACA", "").lower() == "true":/' environment/bot/credentials.py
+      ;;
+    *) 
+    exit 1
+    ;;  
+  esac
+fi
+
+sudo docker compose up --remove-orphans
--- environment/healthcheck.py
+++ environment/healthcheck.py
@@ -1,13 +1,4 @@
-import os
-from datetime import datetime, timedelta
-
-import pandas as pd
-import pandas_ta as ta
-import pytz
-from lumibot.entities import Asset, TradingFee
 from lumibot.strategies.strategy import Strategy
-import yfinance as yf
-
 from credentials import broker
 
 class ConnectionTest(Strategy):
@@ -15,9 +6,9 @@ class ConnectionTest(Strategy):
         dt = self.get_datetime()
 
         if dt is None:
+            self.log_message("-------------------------broken")
             exit(1)
         exit(0)
-
 
 strategy = ConnectionTest(
     broker=broker,
