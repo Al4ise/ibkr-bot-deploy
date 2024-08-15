184832146714b91e097dc43d133a89a4fed02d5b
Martin Pelteshki <39273158+Al4ise@users.noreply.github.com>
2024-08-15 14:11:16 +0300

    ib_subaccount

--- setup.sh
+++ setup.sh
@@ -29,33 +29,42 @@ setupStrategies(){
 
         read -rp "Enter your choice: " choice
 
-        if [ "$choice" == "1" ]; then
-            read -rp "Strategy Name: " strategy_name
-            read -rp "Trading Mode (live or paper): " live_or_paper
-            read -rp "Strategy GitHub Repo: " bot_repo
-            read -rp "Database String (Optional): " db_str
-            read -rp "Strategy Config File (Optional): " config_file
-            read -rp "Discord Webhook URL (Optional): " webhook
-            
-            echo "${strategy_name,,},$live_or_paper,$bot_repo,$db_str,$config_file,$webhook" >> environment/.pref
+        case $choice in
+            1)
+                read -rp "Strategy Name: " strategy_name
+                read -rp "Trading Mode (live or paper): " live_or_paper
+                read -rp "Strategy GitHub Repo: " bot_repo
+                read -rp "Database String (Optional): " db_str
+                read -rp "Strategy Config File (Optional): " config_file
+                read -rp "Discord Webhook URL (Optional): " webhook
+                read -rp "IB Subaccount (Optional): " ib_subaccount
+                
+                echo "${strategy_name,,},$live_or_paper,$bot_repo,$db_str,$config_file,$webhook,$ib_subaccount" >> environment/.pref
+                ;;
 
-        elif [ "$choice" == "2" ]; then
-            echo "WIP"
+            2)
+                echo "WIP"
+                ;;
 
-        elif [ "$choice" == "3" ]; then
-            rm -f "environment/.cred"
-            echo "[*] Credentials Reset"
+            3)
+                rm -f "environment/.cred"
+                echo "[*] Credentials Reset"
+                ;;
 
-        elif [ "$choice" == "4" ]; then
-            rm -f "environment/.pref"
-            echo "[*] Strategies Reset"
+            4)
+                rm -f "environment/.pref"
+                echo "[*] Strategies Reset"
+                ;;
 
-        elif [ "$choice" == "5" ]; then
-            return
-        else
-            echo "Invalid choice. Exiting..."
-            exit 1
-        fi
+            5)
+                return
+                ;;
+
+            *)
+                echo "Invalid choice. Exiting..."
+                exit 1
+                ;;
+        esac
     done
 }
 
